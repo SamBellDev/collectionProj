023e3448a0d0d963d20e7937e25b0a2e9ffa6594
Sam Bell <sambelldev@tuta.io>
2021-08-02 12:01:43 +0100

    First release of basic HTML frame

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+?>
+<html lang="en">
+<head>
+    <title>My collection</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+<h1>My collection</h1>
+<form>
+    <label>
+        New Item:
+        <input type="text" name="newItem">
+    </label>
+    <label>
+        Stat 1:
+        <input type="text" name="stat1">
+    </label>
+    <label>
+        Stat 2:
+        <input type="text" name="stat2">
+    </label>
+    <label>
+        Stat 3:
+        <input type="text" name="stat3">
+    </label>
+    <input type="submit" name="submit">
+</form>
+<p> Item 1
+    <ul>
+        <li>stat 1
+        </li>
+        <li>stat 2
+        </li>
+        <li>stat 3
+        </li>
+    </ul>
+</p>
+<p> Item 2
+<ul>
+    <li>stat 1
+    </li>
+    <li>stat 2
+    </li>
+    <li>stat 3
+    </li>
+</ul>
+</p>
+<p> Item 3
+<ul>
+    <li>stat 1
+    </li>
+    <li>stat 2
+    </li>
+    <li>stat 3
+    </li>
+</ul>
+</p>
+</body>
+
+</html>
