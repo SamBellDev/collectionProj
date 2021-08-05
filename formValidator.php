0acd8280e45e736f3201dcff9f5146b8414ea05b
Sam Bell <sambelldev@tuta.io>
2021-08-05 10:13:37 +0100

    changed some repeated code in the validator to be cleaner.

--- formValidator.php
+++ formValidator.php
@@ -3,16 +3,14 @@
 session_start();
 if(!isset($_POST)) {
     session_unset();
-    header('Location: index.php');
 } else {
     if ($_POST['coinName'] !='' && $_POST['yearMinted'] !='' && $_POST['material'] !='' && $_POST['diameter'] !=''){
         $_SESSION['coinName'] = $_POST['coinName'];
         $_SESSION['yearMinted'] = $_POST['yearMinted'];
         $_SESSION['material'] = $_POST['material'];
         $_SESSION['diameter'] = $_POST['diameter'];
-        header('Location: index.php');
     } else {
         session_abort();
-        header('Location: index.php');
     }
 }
+header('Location: index.php');
